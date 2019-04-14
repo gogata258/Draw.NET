046fe7a0bf4aa2bb4f66828a3c207c34cf216084
George Anastasov <georgeanastasov@outlook.com>
2019-04-14 13:39:41 +0300

    made elipse shape to use a filled curve

---
+++ Source/Shapes/Abstracts/ClosedCurveBase.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+
+namespace Draw.Shapes.Abstracts
+{
+	public class ClosedCurveBase : ShapeBase
+	{
+		[JsonConstructor] protected ClosedCurveBase() { }
+		public ClosedCurveBase(ShapeBase shape, string name, string type) : base(shape, name, type) { }
+		public ClosedCurveBase(float X, float Y, float width, float height, string name, string type) : base(X, Y, width, height, name, type) { }
+
+		public override void DrawSelf(Graphics grfx)
+		{
+			PointF[] drawPoints = GetNormalizedPoints( ).ToArray( );
+			GetTransformationMatrix( ).TransformPoints(drawPoints);
+
+			if (BorderAlpha > 0 && BorderThickness > 0)
+				grfx.DrawClosedCurve(new Pen(BorderColor, BorderThickness), drawPoints, .8f, FillMode.Alternate);
+
+			if (FillAlpha > 0)
+				grfx.FillClosedCurve(new SolidBrush(FillColor), drawPoints, FillMode.Alternate, .8f);
+
+			base.DrawSelf(grfx);
+		}
+	}
+}
--- Source/Shapes/Elipse.cs
+++ Source/Shapes/Elipse.cs
@@ -4,27 +4,21 @@ using System.Drawing.Drawing2D;
 
 namespace Draw.Shapes
 {
+	using System.Collections.Generic;
 	using Abstracts;
 
-	public class Elipse : ShapeBase
+	public class Elipse : ClosedCurveBase
 	{
 		[JsonConstructor] private Elipse() { }
 		public Elipse(ShapeBase shape, string name) : base(shape, name, typeof(Elipse).Name) { }
 		public Elipse(float X, float Y, float width, float height, string name) : base(X, Y, width, height, name, typeof(Elipse).Name) { }
 
-		public override void DrawSelf(Graphics grfx)
+		protected override List<PointF> GetNormalizedPoints() => new List<PointF>( )
 		{
-			var matrix = new Matrix();
-			matrix.Translate(LocationX, LocationY);
-			matrix.RotateAt(Rotation, MedianPoint);
-
-			//TODO USE THIS INSTEAD
-			//grfx.DrawClosedCurve()
-
-			grfx.Transform = matrix;
-			grfx.DrawEllipse(new Pen(new SolidBrush(BorderColor)), BorderBoundingBox);
-			grfx.FillEllipse(new SolidBrush(FillColor), ObjectBoundingBox);
-			base.DrawSelf(grfx);
-		}
+			new PointF(-0.5f, 0.5f),
+			new PointF(0.5f, 0.5f),
+			new PointF(0.5f, -0.5f),
+			new PointF(-0.5f, -0.5f)
+		};
 	}
 }
